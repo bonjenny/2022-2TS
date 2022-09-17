2a3d4174f242c79f5b3ccd2450a5f220ecb2d7a7
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-09-17 15:58:15 +0900

    9/17 - webpack download and task start

--- code/react/src/App.js
+++ code/react/src/App.js
@@ -1,4 +1,5 @@
 import Todo from './components/Todo';
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1>My Todos</h1>
       <Todo text='Learn React' />
       <Todo text='섹션 1 학습' />
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
