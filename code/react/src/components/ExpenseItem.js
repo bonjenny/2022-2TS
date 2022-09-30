6bb08ad75d9f5b8cbf82e0d4e127a98a3a17c341
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-09-30 14:32:39 +0900

    9/30 - 3강까지 수강 (과제 전)

--- code/react/src/components/ExpenseItem.js
+++ code/react/src/components/ExpenseItem.js
@@ -1,15 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) { // not ExpenseItem(title, amount, date)
-	// regular JS
-	const expenseDate = new Date(2021, 2, 28); // 2021년 3월 28일
-	const expenseTitle = 'Car Insurance';
-	const expenseAmount = 294.67;
+function ExpenseItem(props) {
 
-	// JSX
 	return (
 		<div className="expense-item">
-			<div>{props.date.toISOString()}</div>
+			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
 				<h2>{props.title}</h2>
 				<div className="expense-item__price">{props.amount}</div>
