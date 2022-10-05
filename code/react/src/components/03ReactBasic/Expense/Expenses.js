49a281ae6b8dd11ee1a2870b7ad706618f963f7f
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-10-05 22:31:12 +0900

    10/5 - 43. 첫번째 요약 - 수강 완료

--- code/react/src/components/03ReactBasic/Expense/Expenses.js
+++ code/react/src/components/03ReactBasic/Expense/Expenses.js
@@ -4,37 +4,16 @@ import "./CSS/Expenses.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
-      {/* {props.items ? props.items.map(c => {
+    <Card className="expenses">
+      {props.items ? props.items.map(c => {
         return (
           <ExpenseItem
             title={c.title}
             amount={c.amount}
             date={c.date} />
         )}) : <ExpenseItem />
-      } */}
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </div>
+      }
+    </Card>
   );
 }
-
 export default Expenses;
