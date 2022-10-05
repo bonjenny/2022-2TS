39a76201d7d88db4094a7419c8806fa047d738af
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-10-05 23:29:48 +0900

    44-45. 요약 및 JSX 정리 - 수강 완료

--- code/react/src/components/03ReactBasic/Expense/ExpenseDate.js
+++ code/react/src/components/03ReactBasic/Expense/ExpenseDate.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './CSS/ExpenseDate.css';
 
 function ExpenseDate(props) {
--- code/react/src/components/03ReactBasic/UI/Card.js
+++ code/react/src/components/03ReactBasic/UI/Card.js
@@ -1,3 +1,4 @@
+import React from "react";
 import './CSS/Card.css'
 
 function Card(props) {
