5c7352be83db7ab5e8d2fd8d099bac7e1bffcd0e
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-10-07 01:48:37 +0900

    섹션 4 state, 양방향 바인딩 수강 완료

--- code/react/src/components/03ReactBasic/Expense/ExpenseItem.js
+++ code/react/src/components/03ReactBasic/Expense/ExpenseItem.js
@@ -1,26 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./CSS/ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  
-  //console.log('ExpenseItem evaluated by React');
-  const [title, setTitle] = useState(props.title); //let title = props.title;
-  
-  const clickHandler = () => { // function clickHandler()
-    setTitle('Updated!');
-    console.log(title); // 콘솔창에 뜸
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
