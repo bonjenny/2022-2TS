468bdec1d8d465277ff321ad8ea29c86315f9a0b
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-10-07 02:55:54 +0900

    70. keys 수강 완료

--- code/react/src/components/03ReactBasic/Expense/Expenses.js
+++ code/react/src/components/03ReactBasic/Expense/Expenses.js
@@ -5,24 +5,28 @@ import Card from "../UI/Card";
 import "./CSS/Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('20020');
+  const [filteredYear, setFilteredYear] = useState("20020");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log(selectedYear)
+    console.log(selectedYear);
   };
 
   return (
-      <Card className="expenses">
-        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items ? props.items.map(c => {
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      {props.items ? (
+        props.items.map((c, idx) => {
           return (
             <ExpenseItem
+              key={c.id}
               title={c.title}
               amount={c.amount}
-              date={c.date} />
-          )}) : <ExpenseItem />
-        }
-      </Card>
+              date={c.date}
+            />
+          );
+        })
+      ) : ( <ExpenseItem /> )}
+    </Card>
   );
-}
+};
 export default Expenses;
