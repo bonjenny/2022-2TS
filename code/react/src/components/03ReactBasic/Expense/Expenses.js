3a83146a33b3bb64699c51278c7c95f50c0a447b
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-10-07 23:36:16 +0900

    10/7 - 75. 조건부내용 - 수강완료

--- code/react/src/components/03ReactBasic/Expense/Expenses.js
+++ code/react/src/components/03ReactBasic/Expense/Expenses.js
@@ -5,27 +5,30 @@ import Card from "../UI/Card";
 import "./CSS/Expenses.css";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("20020");
+  const [filteredYear, setFilteredYear] = useState("2021");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
     console.log(selectedYear);
   };
+  
+  const filteredExpense = props.items.filter(c =>
+    c.date.getFullYear().toString() === filteredYear);
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpense.length > 0) {
+    expensesContent = filteredExpense.map((c, idx) => {
+      return <ExpenseItem key={c.id} 
+        title={c.title} amount={c.amount} date={c.date} />
+    })
+  }
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-      {props.items ? (
-        props.items.map((c, idx) => {
-          return (
-            <ExpenseItem
-              key={c.id}
-              title={c.title}
-              amount={c.amount}
-              date={c.date}
-            />
-          );
-        })
-      ) : ( <ExpenseItem /> )}
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {expensesContent}
     </Card>
   );
 };
--- code/react/src/components/03ReactBasic/NewExpense/ExpenseForm.js
+++ code/react/src/components/03ReactBasic/NewExpense/ExpenseForm.js
@@ -46,42 +46,62 @@ const ExpenseForm = (props) => {
     setEnteredDate('');
   };
 
+  const cancelHandler = (event) => {
+    event.preventDefault();
+    setFormContent(0);
+  }
+
+  const [formContent, setFormContent] = useState(0);
+  const addNewExpenseHandler = (event) => {
+    event.preventDefault();
+    setFormContent(1);
+  };
+
   return (
-    <form onSubmit={submitHandler}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={enteredTitle}
-            onChange={titleChangeHandler}
-          />
+    <div>
+      {formContent === 1 ?
+        <form onSubmit={submitHandler}>
+        <div className="new-expense__controls">
+          <div className="new-expense__control">
+            <label>Title</label>
+            <input
+              type="text"
+              value={enteredTitle}
+              onChange={titleChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input
+              type="number"
+              min="0.01"
+              step="0.01"
+              value={enteredAmount}
+              onChange={amountChangeHandler}
+            />
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              value={enteredDate}
+              onChange={dateChangeHandler}
+            />
+          </div>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            min="0.01"
-            step="0.01"
-            value={enteredAmount}
-            onChange={amountChangeHandler}
-          />
+        <div className="new-expense__actions">
+          <button type="cancel" onClick={cancelHandler}>Cancel</button>
+          <button type="submit">Add Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={enteredDate}
-            onChange={dateChangeHandler}
-          />
-        </div>
-      </div>
-      <div className="new-expense__actions">
-        <button type="submit">Add Expense</button>
-      </div>
-    </form>
+      </form>
+    : 
+      <form onSubmit={addNewExpenseHandler}>
+        <button type="submit">Add New Expense</button>
+      </form>
+    }
+  </div>
   );
 };
 export default ExpenseForm;
