24e7c1799422350d5ef7346609b3e4130b0cff15
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-10-08 01:10:51 +0900

    76. 조건 명령문 반환

--- code/react/src/components/Expense/Expenses.js
+++ code/react/src/components/Expense/Expenses.js
@@ -1,26 +1,18 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem";
-import ExpensesFilter from "./ExpensesFilter";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./CSS/Expenses.css";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2021");
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log(selectedYear);
   };
   
-  const filteredExpense = props.items.filter(c =>
-    c.date.getFullYear().toString() === filteredYear);
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpense.length > 0) {
-    expensesContent = filteredExpense.map((c, idx) => {
-      return <ExpenseItem key={c.id} 
-        title={c.title} amount={c.amount} date={c.date} />
-    })
-  }
+  const filteredExpense = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <Card className="expenses">
@@ -28,7 +20,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {expensesContent}
+      <ExpensesList items={filteredExpense} />
     </Card>
   );
 };
