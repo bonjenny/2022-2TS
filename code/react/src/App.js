f465b9b7e7dac73652b928e33b6e5e106aa85d21
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-10-14 01:36:57 +0900

    섹션 10: 시작하기

--- code/react/src/App.js
+++ code/react/src/App.js
@@ -1,41 +1,31 @@
-import React, { useState } from "react";
-import AddUser from "./components/8-9_AddUser/Users/AddUser";
-import UsersList from "./components/8-9_AddUser/Users/UsersList";
-// import Wrapper from "./components/8-9_AddUser/Helpers/Wrapper";
-import "./App.css";
+import React, { useState } from 'react';
 
-const App = () => {
-  const [usersList, setUsersList] = useState([
-    { name: "Max", age: "31", id: "a1" },
-    { name: "Jenny", age: "21", id: "a2" },
-  ]);
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [...prevUsersList, {
-        name: uName,
-        age: uAge,
-        id: Math.random().toString(),
-      }];
-    });
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
   };
 
-  const deleteUserHandler = (userId) => {
-    setUsersList((prevUsers) => {
-      const updatedUsers = prevUsers.filter((user) => user.id !== userId);
-      return updatedUsers;
-    });
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
   };
 
   return (
-    <>
-      <section id="user-form">
-        <AddUser onAddUser={addUserHandler} />
-      </section>
-      <section id="users">
-        <UsersList users={usersList} onDeleteUser={deleteUserHandler} />
-      </section>
-    </>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
-};
+}
+
 export default App;
