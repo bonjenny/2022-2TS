35aef14c6efe44f02577bed521e3d0b2976d01b8
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-11-04 19:56:27 +0900

    섹션 11: Food Order App 완료

--- code/react/src/App.js
+++ code/react/src/App.js
@@ -1,28 +1,12 @@
-import { useState } from "react";
+import React from 'react';
 
-import Cart from "./components/11_FoodOrder/Cart/Cart";
-import Header from "./components/11_FoodOrder/Layout/Header";
-import Meals from "./components/11_FoodOrder/Meals/Meals";
-import CartProvider from "./store/11_FoodOrder/CartProvider";
+import './App.css';
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
-
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
+    <div className="app">
+      <h1>Hi there!</h1>
+    </div>
   );
 }
 
