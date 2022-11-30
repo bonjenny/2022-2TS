98cd4ffec1a2152580bbf016f1f520237e1dc70c
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-11-30 16:52:20 +0900

    175. state 및 이벤트 작업하기

--- code/react/src/components/13_ErrorBoundary/Users.js
+++ code/react/src/components/13_ErrorBoundary/Users.js
@@ -1,6 +1,5 @@
-import { useState } from 'react';
+import { Component } from 'react';
 import User from './User';
-
 import classes from './CSS/Users.module.css';
 
 const DUMMY_USERS = [
@@ -9,29 +8,37 @@ const DUMMY_USERS = [
   { id: 'u3', name: 'Julie' },
 ];
 
-const Users = () => {
-  const [showUsers, setShowUsers] = useState(true);
-
-  const toggleUsersHandler = () => {
-    setShowUsers((curState) => !curState);
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = { showUsers: true };
   };
 
-  const usersList = (
-    <ul>
-      {DUMMY_USERS.map((user) => (
-        <User key={user.id} name={user.name} />
-      ))}
-    </ul>
-  );
+  toggleUsersHandler() {
+    // this.state.showUsers = false; // NOT!
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers };
+    });
+  };
 
-  return (
-    <div className={classes.users}>
-      <button onClick={toggleUsersHandler}>
-        {showUsers ? 'Hide' : 'Show'} Users
-      </button>
-      {showUsers && usersList}
-    </div>
-  );
+  render() {
+    const usersList = (
+      <ul>
+        {DUMMY_USERS.map((user) => (
+          <User key={user.id} name={user.name} />
+        ))}
+      </ul>
+    );
+
+    return (
+      <div className={classes.users}>
+        <button onClick={this.toggleUsersHandler.bind(this)}>
+          {this.state.showUsers ? 'Hide' : 'Show'} Users
+        </button>
+        {this.state.showUsers && usersList}
+      </div>
+    );
+  };
 };
 
 export default Users;
