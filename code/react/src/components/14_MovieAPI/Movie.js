5a7afddd9862f5db2e59898f69d6ff9a4b494569
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-01-04 19:12:58 +0900

    187. 비동기/대기 사용하기

--- code/react/src/components/14_MovieAPI/Movie.js
+++ code/react/src/components/14_MovieAPI/Movie.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React from "react";
 
-import classes from './CSS/Movie.module.css';
+import classes from "./CSS/Movie.module.css";
 
 const Movie = (props) => {
   return (
